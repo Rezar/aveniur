1b61fbf43fb82964cbf0ecfe4a752190f1c0efa9
Chibundom Ejimuda <chibundomejimuda@gmail.com>
2024-06-18 17:49:42 -0400

    latest changes..

--- Frontend/sign_in.py
+++ Frontend/sign_in.py
@@ -1,10 +1,16 @@
 import streamlit as st
 from StreamlitGauth.google_auth import Google_auth
+# import warnings
+
+# # Ignore all warnings
+# warnings.filterwarnings("ignore")
+
+st.logo("images/lavenir.PNG")
 
 def main():
     client_id = "166960327947-gslpqkh5091ppc7paa4fj05sokjktv37.apps.googleusercontent.com"
     client_secret = "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"
-    redirect_uri = "http://localhost:8501/home"  # Correcting the URI
+    redirect_uri = "http://128.197.206.144:8501/home"
 
     login = Google_auth(clientId=client_id, clientSecret=client_secret, redirect_uri=redirect_uri)
 
